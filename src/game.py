d0901f318503954366a5e7781684e6fdee23a4d0
MacBook Air <macbookair@MacBook-Pro-MacBook-3.local>
2026-04-08 17:46:04 +0200

    Added class game

---
+++ src/game.py
@@ -0,0 +1,26 @@
+from stages import STAGES
+
+class Game:
+    def __init__(self, word):
+        self.word = word
+        self.guessed = ["_"] * len(word)
+        self.attempts = 6
+
+    def guess(self, letter):
+        if letter in self.word:
+            for i, l in enumerate(self.word):
+                if l == letter:
+                    self.guessed[i] = letter
+            return True
+        else:
+            self.attempts -= 1
+            return False
+    
+    def is_won(self):
+        return "_" not in self.guessed
+    
+    def is_lost(self):
+        return self.attempts <= 0
+    
+    def display_hangman(self):
+        return STAGES[6 - self.attempts]
