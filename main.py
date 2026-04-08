48c301dc50a6673bf2fdc5cd1d10f80dfd7d8095
MacBook Air <macbookair@MacBook-Pro-MacBook-3.local>
2026-04-08 17:57:03 +0200

    Added game logic

---
+++ main.py
@@ -0,0 +1,33 @@
+from src.player import Player
+from src.game import Game
+from src.word_loader import WordLoader
+from src.scoreboard import Scoreboard
+from src.stages import STAGES
+
+loader = WordLoader("data/words.txt")
+word = loader.get_random_word()
+
+game = Game(word)
+player = Player(input("Your name: "))
+
+while not game.is_won() and not game.is_lost():
+    print("Word:", " ".join(game.guessed))
+    print("Attempts left: ", game.attempts)
+
+    letter = input("Guess letter : ")
+
+    if not letter.isalpha():
+        print("Invalid input")
+        continue
+
+    game.guess(letter)
+
+print(game.display_hangman())
+
+if game.is_won():
+    print("You won!")
+    player.add_score(10)
+else:
+    print("You lost, word was: ", word)
+
+Scoreboard().save(player.name, player.score)
